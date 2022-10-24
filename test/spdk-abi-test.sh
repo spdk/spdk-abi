6361db7a88551025103bda33e67e2d9ca2dbe6e8
Kamil Godzwon <kamilx.godzwon@intel.com>
2022-10-24 05:38:49 -0400

    test: add spdk-abi-test script
    Compare user-generated XML files with automatically
    generated files from CI. This script will be used
    by spdk-abi per patch to confirm that uploaded XML
    files are correctly created.
    
    Signed-off-by: Kamil Godzwon <kamilx.godzwon@intel.com>
    Change-Id: I2b24d2ecf4acfc3051375e428e9e2e9490439c9d
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk-abi/+/15126
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

---
+++ test/spdk-abi-test.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#  SPDX-License-Identifier: BSD-3-Clause
+#  Copyright (C) 2022 Intel Corporation.
+#  All rights reserved.
+#
+
+set -e
+spdk_url="https://review.spdk.io/spdk/spdk.git"
+
+rootdir=$(readlink -f $(dirname $(readlink -f $0))/..)
+tag_pattern="v[0-9][0-9]\.[0-9][0-9]"
+
+trap 'rm -rf $rootdir/v*_test' EXIT SIGINT
+for branch in $(ls $rootdir | grep -Eo "${tag_pattern}\.x"); do
+	# Read hash from revision and get spdk_tag which it corresponds to
+	hash=$(cat $rootdir/$branch/revision)
+	# Get spdk_tag from the remote using commit hash from spdk-abi generated branch SO files
+	spdk_tag=$(git ls-remote $spdk_url | grep $hash | grep -Po "${tag_pattern}.*(?=\^)")
+
+	# Generate XML files and check if they differ from the user uploaded
+	$rootdir/generate_xml_abi.sh $spdk_tag "$rootdir/${branch}_test"
+	for lib in "$rootdir/${branch}_test"/libspdk_*.so; do
+		lib_name=$(basename "$lib")
+		if ! abidiff "$lib" "$rootdir/$branch/$lib_name"; then
+			echo "$spdk_tag verification failed"
+			exit 1
+		fi
+	done
+	echo "Veryfing $spdk_tag completed - success"
+done
