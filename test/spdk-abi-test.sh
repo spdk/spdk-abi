bc2ae2eccf5aa1c3d8f6fd416e95418486f81e1c
Konrad Sztyber <konrad.sztyber@intel.com>
2024-07-11 12:19:44 +0200

    test: clone the repository once
    There's no need to clone it for each tag.
    
    Change-Id: I8ba0fe310b34990ad0fc4528a19d204dd52034b3
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk-abi/+/24137
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>

--- test/spdk-abi-test.sh
+++ test/spdk-abi-test.sh
@@ -7,14 +7,18 @@
 set -e
 
 rootdir=$(readlink -f $(dirname $(readlink -f $0))/..)
+repodir="$rootdir/test/spdk"
 tag_pattern="v[0-9][0-9]\.[0-9][0-9]"
 
-trap 'rm -rf $rootdir/v*_test' EXIT SIGINT
+trap 'rm -rf "$rootdir"/v*_test "$repodir"' EXIT SIGINT
+git clone --recursive "https://github.com/spdk/spdk" "$repodir"
+
 for branch in $(ls $rootdir | grep -Eo "${tag_pattern}\.x"); do
 	tag="${branch%*.x}"
 
 	# Generate XML files and check if they differ from the user uploaded
-	$rootdir/generate_xml_abi.sh -t "$tag" -x "$rootdir/${branch}_test"
+	$rootdir/generate_xml_abi.sh -t "$tag" -x "$rootdir/${branch}_test" \
+		--spdk-path="$repodir" --force-build --force-checkout
 	for lib in "$rootdir/${branch}_test"/libspdk_*.so; do
 		lib_name=$(basename "$lib")
 		if ! abidiff "$lib" "$rootdir/$branch/$lib_name"; then
